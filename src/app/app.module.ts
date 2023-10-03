ffbf12df154cd51945b9596e75421feb1cd40b54
Henrique Zagonel Pereira <henriquezpereira@hotmail.com>
2023-10-02 21:24:48 -0300

    components createds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardsComponent } from './components/small-cards/small-cards.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardsComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
